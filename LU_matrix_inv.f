90a17a331954b47e98ef36a8487c8b5448faf53d
Dmitry Martyanov <martyanov@gmail.com>
2016-11-04 14:18:43 +0300

    try to evaluate matrix inversion accuracy

--- LU_matrix_inv.f
+++ LU_matrix_inv.f
@@ -6,8 +6,12 @@ C     *******************************************************
       complex*16,allocatable,dimension(:,:) :: A_MAT
       complex*16,allocatable,dimension(:)   :: WORK
       integer,allocatable,dimension(:)      :: IPIV
-      integer i,j,info,error,k
-
+      integer i,j,info,error,k, KJ
+      
+      real*16 CRRR,CIII,DDD, QA_MATI, QA_MATR
+      complex*16 MYY
+      
+      
       INCLUDE 'PRIVCOM.FOR'
       
       M=NCLL
@@ -43,6 +47,35 @@ C     *******************************************************
        write(*,*)"!!!!!!!!!!!!!!!!!ZGETRI failed"
       end if
   
+c     CRRR=0.Q0
+c     CIII=0.Q0
+c     DDD=0.Q0
+c     MYY=dcmplx(0,0)
+c     DO j=1,NCLL
+c         DO i=1,NCLL
+c             DO k=1,NCLL
+c                 KJ=(k-1)*NCLL+j
+c                   C=C+A_MAT(i,k)*A(k,j)
+c                 QA_MATR=DBLE(A_MAT(i,k))
+c                 QA_MATI=DIMAG(A_MAT(i,k))
+c                 CRRR=CRRR+QA_MATR*ABR(KJ)
+c    *                 -QA_MATI*ABI(KJ)
+c                 CIII=CIII+QA_MATR*ABI(KJ)
+c    *                 +QA_MATI*ABR(KJ)
+c                 MYY = MYY + A_MAT(i,k)*dcmplx(ABR(KJ),ABI(KJ))
+c             END DO
+c             IF(i.eq.j) CRRR=CRRR-1.Q0
+c             IF(i.eq.j) MYY=MYY-dcmplx(1,0)
+c             DDD = DDD + CDABS(MYY)!QABS(CRRR)+QABS(CIII)!CRRR*CRRR + CIII*CIII
+c             CRRR=0.Q0
+c             CIII=0.Q0
+c         END DO
+c     END DO
+c     
+c     PRINT *, '!!!INVERSION ACCURACY = ', DDD
+
+      
+      
       DO 2 K=1,NCLL
        K1=(K-1)*NCLL
        DO 2 L=1,NCLL
@@ -50,7 +83,7 @@ C     *******************************************************
         ABR(KL)=REAL(A_MAT(K,L))
         ABI(KL)=AIMAG(A_MAT(K,L))
     2 CONTINUE       
-      
+
       deallocate(A_MAT,IPIV,WORK,stat=error)
       if (error.ne.0)then
         print *,"!!!!!!!!!!!!!!!!!!!error:fail to release"
